c2537146463421b79b081107e57c7486f913eb55
Jitendra Kumar Shah <jitendra.kumar.shah@sap.com>
2024-05-12 22:39:39 +0530

    fix

---
+++ app/middelware/verify-jwt.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+
+const authkey = require('../config/jwt-key');
+
+function verifyJWT(req, res, next) {
+
+    const bearerToken = req.headers['authorization'];
+
+    // console.log("token",req.headers)
+
+    if (typeof bearerToken !== 'undefined') {
+
+        const bearer = bearerToken.split(" ");
+
+        const token = bearer[1];
+
+        jwt.verify(token, authkey.KEY, (err, result) => {
+
+            if (err) {
+                res.status(400).send({
+                    message: "Error Found " + err.message,
+                    status: false,
+                    data: []
+                });
+            } else {
+                req.user = result.adminDetails;
+                next();
+            }
+        });
+
+    } else {
+        res.send({
+            message: "Please Provide Bearer Token",
+            status: false,
+            data: []
+        });
+    }
+
+}
+
+module.exports = verifyJWT;
